0cee1675dfb9590ccb5c1d411d2f80fdc546a154
Lohith Srikanth <srikanthlohith@gmail.com>
2021-04-04 12:34:54 +0530

    Modify id property from Number to String

--- src/services/cohortService.js
+++ src/services/cohortService.js
@@ -1,8 +1,8 @@
 export const cohorts = [
-  { _id: 1, name: "Apex" },
-  { _id: 2, name: "Pinnacle" },
-  { _id: 3, name: "Zenith" },
-  { _id: 4, name: "Summit" },
+  { _id: "1", name: "Apex" },
+  { _id: "2", name: "Pinnacle" },
+  { _id: "3", name: "Zenith" },
+  { _id: "4", name: "Summit" },
 ];
 
 export function getCohorts() {
