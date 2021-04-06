92dc2750723990e2264372d7c895f5e1355a1954
Lohith Srikanth <srikanthlohith@gmail.com>
2021-04-06 15:58:48 +0530

    Use the Navbar component to display the navigation bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import "./App.css";
 import Students from "./components/students";
 import { Route, Redirect, Switch } from "react-router-dom";
+import Navbar from "./components/common/navbar";
 
 function App() {
   return (
-    <div className="container" style={{ marginTop: 40 }}>
+    <div className="container" style={{ marginTop: 20 }}>
+      <Navbar />
       <Switch>
         <Route path="/students" component={Students} />
         <Redirect path="/" exact to="/students" />
