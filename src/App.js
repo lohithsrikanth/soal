34860516aa7c27aa3ebac6b7dea2237d9cce3bb4
Lohith Srikanth <srikanthlohith@gmail.com>
2021-04-06 15:27:29 +0530

    Establish routes for /students and / paths

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import React from "react";
 import "./App.css";
 import Students from "./components/students";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="container" style={{ marginTop: 40 }}>
-      <Students />
+      <Switch>
+        <Route path="/students" component={Students} />
+        <Redirect path="/" exact to="/students" />
+      </Switch>
     </div>
   );
 }
