e0f6c7475264bad52e566e7f01200fbb6d95d2fb
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-15 17:47:18 +0530

    Refine implementation of the LoginForm component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  state = {};
+  state = {
+    account: { username: "", password: "" },
+  };
 
-  username = React.createRef();
-
-  componentDidMount() {
-    this.username.current.focus();
-  }
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault();
 
     // Call the server
-    const username = this.username.current.value;
     console.log("Submitted");
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -24,7 +31,9 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
+              name="username"
               id="username"
               type="text"
               className="form-control"
@@ -32,7 +41,14 @@ class LoginForm extends Component {
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
