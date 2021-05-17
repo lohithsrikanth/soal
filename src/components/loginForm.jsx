e7659fc8129c190c3d87834248e59fcd57fff947
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-17 20:07:25 +0530

    Use the Reusable Form component in the LoginForm component and make corresponding changes in the LoginForm component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Input from "./common/input";
-import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
     data: { username: "", password: "" },
     errors: {},
@@ -13,45 +13,8 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.data, this.schema, options);
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({
-      errors: errors || {},
-    });
-    if (errors) return;
-    // Call the server
+  doSubmit = () => {
     console.log("Submitted");
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateInput(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const data = { ...this.state.data };
-    data[input.name] = input.value;
-    this.setState({ data, errors });
-  };
-
-  validateInput = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
   };
 
   render() {
