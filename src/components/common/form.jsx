da61c8b59f59e555ba4e14f009da9848f39352bc
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-17 21:41:22 +0530

    Create the methods renderInput and renderButton

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,4 +1,5 @@
 import React, { Component, useCallback } from "react";
+import Input from "./input";
 import Joi from "joi-browser";
 
 class Form extends Component {
@@ -48,6 +49,29 @@ class Form extends Component {
     data[input.name] = input.value;
 
     this.setState({ data, errors });
+  };
+
+  renderButton = (label) => {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  };
+
+  renderInput = (name, label, type = "text") => {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        label={label}
+        value={data[name]}
+        error={errors[name]}
+        onChange={this.handleChange}
+      />
+    );
   };
 }
 
