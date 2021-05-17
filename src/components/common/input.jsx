f1760b514da6ee4abe7ceda66ebaeafcc4f9b547
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-17 21:43:53 +0530

    Use the rest operator to make the code concise

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 
 const Input = (props) => {
-  const { name, label, value, error, onChange } = props;
+  const { name, label, error, ...rest } = props;
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
