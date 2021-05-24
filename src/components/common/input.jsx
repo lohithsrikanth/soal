d7ba5c3d96fc6e5ffc51f86145f70d06175e8473
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-24 13:06:32 +0530

    Add the Autofocus feature

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -2,6 +2,21 @@ import React from "react";
 
 const Input = (props) => {
   const { name, label, error, ...rest } = props;
+  if (name === "username") {
+    return (
+      <div className="form-group">
+        <label htmlFor={name}>{label}</label>
+        <input
+          {...rest}
+          name={name}
+          id={name}
+          className="form-control"
+          autoFocus
+        />
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    );
+  }
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
