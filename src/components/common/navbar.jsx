9d676c828ce441353de107ee7985fcc309c14ad9
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-24 13:05:50 +0530

    Display certain features of the Navbar only if the user is logged in and display the Logout link when the user is logged out

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout, getUser } from "../../store/users";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <nav
       className="navbar navbar-expand-lg navbar-light bg-light"
@@ -25,22 +27,47 @@ const Navbar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink className="nav-link" to="/students">
               Students <span className="sr-only">(current)</span>
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">
-              Login <span className="sr-only">(current)</span>
-            </NavLink>
-          </li>
+          {!user && (
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            </React.Fragment>
+          )}
         </ul>
+        {user && (
+          <span
+            style={{ marginLeft: 700, color: "black" }}
+            className="navbar-text"
+          >
+            Hello, {user.username}
+          </span>
+        )}
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  user: getUser(state),
+});
+
+export default connect(mapStateToProps)(Navbar);
