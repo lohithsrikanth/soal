5a3f21432f978194181ffe070e4c1e4989a30d8b
Lohith Srikanth <srikanthlohith@gmail.com>
2021-05-24 12:51:47 +0530

    Configure the Redux store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
+
+store.subscribe(() => {
+  console.log("An action has been dispatched!");
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
